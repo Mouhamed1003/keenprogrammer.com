8bcd2b502f96d97d62dd7b9beb7f5f8c9a0b053c
DIOUF MOHAMADOU <144717313+Mouhamed1003@users.noreply.github.com>
2024-05-25 23:07:24 +0100

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,9 +7,9 @@ import { FaFacebook } from "react-icons/fa6";
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://linkedin.com" target='_blank' rel="noreferrer"><FaLinkedin /></a>
-        <a href="https://github.com" target='_blank' rel="noreferrer"><FaGithub /></a>
-        <a href="https://facebook.com" target='_blank' rel="noreferrer"><FaFacebook /></a>
+        <a href="https://www.linkedin.com/in/mohamadou-diouf-974b90218/" target='_blank' rel="noreferrer"><FaLinkedin /></a>
+        <a href="https://github.com/Mouhamed1003/keenprogrammer.com" target='_blank' rel="noreferrer"><FaGithub /></a>
+        <a href="https://web.facebook.com/profile.php?id=100009399596087" target='_blank' rel="noreferrer"><FaFacebook /></a>
     </div>
   )
 }
