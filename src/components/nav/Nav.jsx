94174c9240c469b7dbb8f4d0b441f69902fc59de
DIOUF MOHAMADOU <144717313+Mouhamed1003@users.noreply.github.com>
2024-05-28 07:54:49 +0100

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,7 +4,7 @@ import { AiOutlineHome } from "react-icons/ai";
 import { IoPersonSharp } from "react-icons/io5";
 import { GiSkills } from "react-icons/gi";
 import { CgListTree } from "react-icons/cg";
-import { PiSlideshowFill } from "react-icons/pi";
+import { MdSwipe } from "react-icons/md";
 import { BiSolidMessageRounded } from "react-icons/bi";
 
 import { useState } from 'react';
@@ -20,7 +20,7 @@ const Nav = () => {
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><GiSkills /> </a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><GrProjects /> </a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><CgListTree /> </a>
-      <a href="#temoignages" onClick={() => setActiveNav('#temoignages')} className={activeNav === '#temoignages' ? 'active' : ''}><PiSlideshowFill /> </a>
+      <a href="#temoignages" onClick={() => setActiveNav('#temoignages')} className={activeNav === '#temoignages' ? 'active' : ''}><MdSwipe /> </a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiSolidMessageRounded /> </a>
     </nav>
     
