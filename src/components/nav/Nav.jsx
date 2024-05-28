581c41233f10936bf5fb13fafcef24528020c294
DIOUF MOHAMADOU <144717313+Mouhamed1003@users.noreply.github.com>
2024-05-28 06:54:36 +0100

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,6 +3,7 @@ import './nav.css'
 import { AiOutlineHome } from "react-icons/ai";
 import { IoPersonSharp } from "react-icons/io5";
 import { GiSkills } from "react-icons/gi";
+import { BsStack } from "react-icons/bs";
 // import { FaPhoenixFramework } from "react-icons/fa6";
 import { BsHypnotize } from "react-icons/bs";
 import { BiSolidMessageRounded } from "react-icons/bi";
@@ -18,9 +19,9 @@ const Nav = () => {
       <a href="#header" onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active' : ''}><AiOutlineHome /> </a>
       <a href="#apropos" onClick={() => setActiveNav('#apropos')} className={activeNav === '#apropos' ? 'active' : ''}><IoPersonSharp /> </a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><GiSkills /> </a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><BsHypnotize /> </a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><BsStack /> </a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><GrProjects /> </a>
-      <a href="#temoignages" onClick={() => setActiveNav('#temoignages')} className={activeNav === '#temoignages' ? 'active' : ''}><SiGroupon /> </a>
+      <a href="#temoignages" onClick={() => setActiveNav('#temoignages')} className={activeNav === '#temoignages' ? 'active' : ''}><BsHypnotize /> </a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiSolidMessageRounded /> </a>
     </nav>
     
