2a62e3e123aae6db6ac3544f899a8657809fb1b4
DIOUF MOHAMADOU <144717313+Mouhamed1003@users.noreply.github.com>
2024-06-03 14:36:19 +0100

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,32 +1,29 @@
-import React, { useState } from 'react';
-import './nav.css';
-import { Link, useLocation } from 'react-router-dom';
+import React from 'react'
+import './nav.css'
 import { AiOutlineHome } from "react-icons/ai";
 import { IoPersonSharp } from "react-icons/io5";
 import { GiSkills } from "react-icons/gi";
 import { CgListTree } from "react-icons/cg";
 import { MdSwipe } from "react-icons/md";
 import { BiSolidMessageRounded } from "react-icons/bi";
+
+import { useState } from 'react';
 import { GrProjects } from "react-icons/gr";
 
 const Nav = () => {
-  const location = useLocation();
-  const [activeNav, setActiveNav] = useState(location.pathname);
-
-  const handleNavClick = (navItem) => {
-    setActiveNav(navItem);
-  };
+  const [activeNav, setActiveNav] = useState('#header');
 
   return (
     <nav>
-      <Link to="/" onClick={() => handleNavClick('/')} className={activeNav === '/' ? 'active' : ''}><AiOutlineHome /></Link>
-      <Link to="/apropos" onClick={() => handleNavClick('/apropos')} className={activeNav === '/apropos' ? 'active' : ''}><IoPersonSharp /></Link>
-      <Link to="/experience" onClick={() => handleNavClick('/experience')} className={activeNav === '/experience' ? 'active' : ''}><GiSkills /></Link>
-      <Link to="/services" onClick={() => handleNavClick('/services')} className={activeNav === '/services' ? 'active' : ''}><GrProjects /></Link>
-      <Link to="/portfolio" onClick={() => handleNavClick('/portfolio')} className={activeNav === '/portfolio' ? 'active' : ''}><CgListTree /></Link>
-      <Link to="/temoignages" onClick={() => handleNavClick('/temoignages')} className={activeNav === '/temoignages' ? 'active' : ''}><MdSwipe /></Link>
-      <Link to="/contact" onClick={() => handleNavClick('/contact')} className={activeNav === '/contact' ? 'active' : ''}><BiSolidMessageRounded /></Link>
+      <a href="#header" onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active' : ''}><AiOutlineHome /> </a>
+      <a href="#apropos" onClick={() => setActiveNav('#apropos')} className={activeNav === '#apropos' ? 'active' : ''}><IoPersonSharp /> </a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><GiSkills /> </a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><GrProjects /> </a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><CgListTree /> </a>
+      <a href="#temoignages" onClick={() => setActiveNav('#temoignages')} className={activeNav === '#temoignages' ? 'active' : ''}><MdSwipe /> </a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiSolidMessageRounded /> </a>
     </nav>
+    
   );
 };
 
