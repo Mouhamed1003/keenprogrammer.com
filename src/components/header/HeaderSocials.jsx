266ee286826d9a4c242e1268d75da38e50590957
DIOUF MOHAMADOU <144717313+Mouhamed1003@users.noreply.github.com>
2024-06-03 12:01:06 +0100

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,7 +8,7 @@ const HeaderSocials = () => {
   return (
     <div className='header_socials'>
         <a href="https://www.linkedin.com/in/mohamadou-diouf-974b90218/" target='_blank' rel="noreferrer"><FaLinkedin /></a>
-        <a href="https://github.com/Mouhamed1003/keenprogrammer.com" target='_blank' rel="noreferrer"><FaGithub /></a>
+        <a href="https://github.com/Mouhamed1003" target='_blank' rel="noreferrer"><FaGithub /></a>
         <a href="https://web.facebook.com/profile.php?id=100009399596087" target='_blank' rel="noreferrer"><FaFacebook /></a>
     </div>
   )
