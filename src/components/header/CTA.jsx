fe58893785c25025d3fd37c32f6892ebfdc89d25
DIOUF MOHAMADOU <144717313+Mouhamed1003@users.noreply.github.com>
2024-06-11 15:26:17 +0100

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-// import CV from '../../images/moncv.pdf';
+import React from 'react' 
+import CV from '../../images/moncv.pdf';
 import { CgSoftwareDownload } from "react-icons/cg";
 import { BiSolidMessageRounded } from "react-icons/bi";
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href="#" className='btn' target='_blank' rel='noreferrer'>My Personal Resume <CgSoftwareDownload className='icon-load'/> </a>
+        <a href={CV} className='btn' target='_blank' rel='noreferrer'>My Personal Resume <CgSoftwareDownload className='icon-load'/> </a>
         <a href="#contact" className='btn btn-primary'>Talk To Me... <BiSolidMessageRounded className='icon-load'/></a>
     </div>
   )
